131841973a4a1d899e53ac61f477b604567d6317
Omer Hazan <omerathome@gmail.com>
2022-07-05 18:31:51 +0200

    live endpoint

--- src/elements.js
+++ src/elements.js
@@ -8,10 +8,10 @@ const galleryNav = document.querySelector(".galleryNav");
 const healthNav = document.querySelector(".health");
 const sendMessageBtn = document.querySelector(".SendBtn");
 const carousel = document.querySelector(".carousel");
-// const statsUrl = "https://fastapionly.herokuapp.com/stats"; // Live url
-const statsUrl = "http://127.0.0.1:8000/stats"; // Local url
-// const BASE_URL = "https://fastapionly.herokuapp.com"; // Live url
-const BASE_URL = "http://127.0.0.1:8000"; // Local url
+const statsUrl = "https://fastapionly.herokuapp.com/stats"; // Live url
+// const statsUrl = "http://127.0.0.1:8000/stats"; // Local url
+const BASE_URL = "https://fastapionly.herokuapp.com"; // Live url
+// const BASE_URL = "http://127.0.0.1:8000"; // Local url
 
 const loader = document.getElementById("loader");
 const span = document.getElementById("close");
