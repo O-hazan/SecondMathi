c312d3e9fbd36b984eaf381063839b6c7819f0b8
Omer Hazan <omerathome@gmail.com>
2022-07-28 16:57:13 +0200

    fixed milstone images

--- build/src/milestones.js
+++ build/src/milestones.js
@@ -1,21 +1,21 @@
 // eslint-disable-next-line no-unused-vars
 const milestones = `<div class=" milstoneDiv container-fluid pt-2 row">
 <div class="card text-left col-sm">
-  <img class="card-img-top mx-0" src="/Resources/milestones/sleep.jpg" alt="" />
+  <img class="card-img-top mx-0" src="/build/Resources/milestones/sleep.jpg" alt="" />
   <div class="card-body">
     <h4 class="card-title">Sleeping in the car</h4>
     <p class="card-text">First time sleeping at aba's car</p>
   </div>
 </div>
 <div class="card text-left col-sm">
-  <img class="card-img-top" src="/Resources/milestones/firstLake.JPG" alt="" />
+  <img class="card-img-top" src="/build/Resources/milestones/firstLake.JPG" alt="" />
   <div class="card-body">
     <h4 class="card-title">In the lake</h4>
     <p class="card-text">First time in the lake</p>
   </div>
 </div>
 <div class="card text-left col-sm">
-  <img class="card-img-top" src="/Resources/milestones/firstRibs.JPG" alt="" />
+  <img class="card-img-top" src="/build/Resources/milestones/firstRibs.JPG" alt="" />
   <div class="card-body">
     <h4 class="card-title">Lambchop</h4>
     <p class="card-text">First time eating ribs</p>
